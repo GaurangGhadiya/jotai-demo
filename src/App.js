0bd64e2f5c6c0108301b6564ed0abe053428c4b0
GaurangGhadiya <gaurangghadiya99@gmail.com>
2022-08-28 23:44:44 +0530

    Setup jotai

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { useAtom } from "jotai";
+import { countAtom } from "./States/Home";
+
 
 function App() {
+    const [count, setCount] = useAtom(countAtom);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {count}
+      <button onClick={() => setCount((c) => c + 1)}>one up</button>
     </div>
   );
 }
